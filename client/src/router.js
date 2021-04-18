dfec675dbef4f4c45114fca1626bf097ac9d3554
gowrishankkar <shankarg.kvsulur@gmail.com>
2021-04-18 11:22:27 +0530

    Vuetify integration done

--- client/src/router.js
+++ client/src/router.js
@@ -14,9 +14,9 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      component: () => import("./views/About.vue")
+      path: "/events",
+      name: "events",
+      component: () => import("./views/Events.vue")
     }
   ]
 });
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/mevn.jpg" width="350" />
-    <HelloWorld msg="Default Boilerplate Template" />
+
+    <Booking msg="Default Booking Template" />
+    <v-btn elevation="2"></v-btn>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Booking from "@/components/Booking.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Booking
   }
 };
 </script>
