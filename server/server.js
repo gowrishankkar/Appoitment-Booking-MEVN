efced31e854becce3653f4a18ef81a13fcb61d07
shankargowri <gowrishankar@squashapps.com>
2021-04-18 09:35:45 +0530

    Mongodb setup done

--- server/server.js
+++ server/server.js
@@ -1,26 +1,50 @@
 // Importing required modules
 const cors = require('cors');
+const config = require("config");
 const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 
 // parse env variables
-require('dotenv').config();
-
-// Configuring port
-const port = process.env.PORT || 9000;
-
+// require('dotenv').config();
 const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// DB Config
+const db = config.get("mongoURI");
+
+
+
+
 
 // Configure middlewares
 app.use(cors());
 app.use(express.json());
 
-app.set('view engine', 'html');
+// app.set('view engine', 'html');
 
 // Static folder
-app.use(express.static(__dirname + '/views/'));
+// app.use(express.static(__dirname + '/views/'));
 
 // Defining route middleware
 app.use('/api', require('./routes/api'));
+
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("mongodb connected..."))
+  .catch(err => console.log("MongoError:",err));
+
+
+// Configuring port
+const port = process.env.PORT || 9001;
+
 
 // Listening to port
 app.listen(port);
